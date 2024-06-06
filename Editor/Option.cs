dcfc1e88dbdb724f6d9c1fd75c8909cb27437cdd
kesera2 <eaglekesera2@gmail.com>
2024-06-06 23:36:23 +0900

    implement showOption<T>

--- Editor/Option.cs
+++ Editor/Option.cs
@@ -1,13 +1,23 @@
-﻿namespace kesera2.FBXOptionsManager
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace kesera2.FBXOptionsManager
 {
 
 
     internal class Option<T>
     {
+        private class OptionT
+        {
+            public T value;
+        }
         private T _value { get; set; }
         private int _toolbarEnable = 0;
         private string _label;
         private string _tooltip;
+        private const int OPTION_WIDTH = 350;
+        private const float INTERVAL_WIDTH = 10;
+        private static GUILayoutOption[] optionsWidth = { GUILayout.Width(OPTION_WIDTH) };
         internal Option(T value, int toolbarEnable, string label, string tooltip = "")
         {
             _value = value;
@@ -35,6 +45,104 @@
         public string Tooltip
         {
             get { return _tooltip; }
+        }
+        private static int drawToggleEnableToolbar(int currentSelection)
+        {
+            return GUILayout.Toolbar(currentSelection, Toolbar.TOOLBAR_LABLE);
+        }
+
+        public static void showOption(Option<float> option)
+        {
+            ShowTOption(option);
+        }
+        public static void showOption(Option<bool> option)
+        {
+            ShowTOption(option);
+        }
+        public static void showOption(Option<MeshOptimizationFlags> option)
+        {
+            ShowTOption(option);
+        }
+        public static void showOption(Option<ModelImporterMeshCompression> option)
+        {
+            ShowTOption(option);
+        }
+
+        public static void showOption(Option<ModelImporterTangents> option)
+        {
+            ShowTOption(option);
+        }
+        public static void showOption(Option<ModelImporterNormalCalculationMode> option)
+        {
+            ShowTOption(option);
+        }
+        public static void showOption(Option<ModelImporterNormals> option)
+        {
+            ShowTOption(option);
+        }
+        public static void showOption(Option<ModelImporterIndexFormat> option)
+        {
+            ShowTOption(option);
+        }
+
+        public static void ShowTOption<T>(Option<T> option)
+        {
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    bool isDisabled = option._toolbarEnable == (int)Toolbar.TOOLBAR.DISABLE;
+                    using (new DisabledColorScope(Color.gray, isDisabled))
+                    {
+                        if (isDisabled)
+                        {
+                            EditorGUILayout.LabelField(option._label, optionsWidth);
+                        }
+                        else
+                        {
+                            UpdateOptionValue(option);
+                        }
+                    }
+                    GUILayout.Space(INTERVAL_WIDTH);
+                    option.ToolbarEnable = drawToggleEnableToolbar(option.ToolbarEnable);
+                }
+            }
+        }
+
+        private static void UpdateOptionValue<T>(Option<T> option)
+        {
+            if (typeof(T) == typeof(float))
+            {
+                option.Value = (T)(object)EditorGUILayout.FloatField(option._label, (float)(object)option._value, optionsWidth);
+            }
+            else if (typeof(T) == typeof(bool))
+            {
+                option.Value = (T)(object)EditorGUILayout.Toggle(option._label, (bool)(object)option._value, optionsWidth);
+            }
+            else if (typeof(T) == typeof(ModelImporterMeshCompression))
+            {
+                option.Value = (T)(object)EditorGUILayout.EnumPopup(option.Label, (ModelImporterMeshCompression)(object)option.Value, optionsWidth);
+            }
+            else if (typeof(T) == typeof(MeshOptimizationFlags))
+            {
+                option.Value = (T)(object)EditorGUILayout.EnumFlagsField(option.Label, (MeshOptimizationFlags)(object)option.Value, optionsWidth);
+            }
+            else if (typeof(T) == typeof(ModelImporterIndexFormat))
+            {
+                option.Value = (T)(object)EditorGUILayout.EnumPopup(option.Label, (ModelImporterIndexFormat)(object)option.Value, optionsWidth);
+            }
+            else if (typeof(T) == typeof(ModelImporterNormals))
+            {
+                option.Value = (T)(object)EditorGUILayout.EnumPopup(option.Label, (ModelImporterNormals)(object)option.Value, optionsWidth);
+            }
+            else if (typeof(T) == typeof(ModelImporterNormalCalculationMode))
+            {
+                option.Value = (T)(object)EditorGUILayout.EnumPopup(option.Label, (ModelImporterNormalCalculationMode)(object)option.Value, optionsWidth);
+            }
+            else if (typeof(T) == typeof(ModelImporterTangents))
+            {
+                option.Value = (T)(object)EditorGUILayout.EnumPopup(option.Label, (ModelImporterTangents)(object)option.Value, optionsWidth);
+            }
         }
     }
 }
