6b58917f1b344f0ecab8bbb064da4a7a75bbb549
kesera2 <eaglekesera2@gmail.com>
2024-11-04 20:58:34 +0900

    Refactor Localization.cs

--- Editor/Function/Localization.cs
+++ Editor/Function/Localization.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,36 +6,45 @@ namespace kesera2.FBXOptionsManager
 {
     public class Localization : ScriptableSingleton<Localization>
     {
+        // 言語選択の列挙型
+        public enum SelectedLanguage
+        {
+            Jp,
+            En
+        }
+
+        // 日・英のアセット名
         private const string AssetNameJa = "JP";
+
         private const string AssetNameEn = "EN";
-        private const string LangAssetFolderPath = "Language/";
+
+        // 言語アセットフォルダへのパス
+        private const string LangAssetFolderPath = "Language";
+
+        // サポートされている言語
         internal static readonly string[] Languages = { "日本語", "English" };
+
+        // 現在選択されている言語のハッシュ
         public static LanguageHash Lang { get; private set; }
 
         public void OnEnable()
         {
-            Lang = (SelectedLanguage)FBXOptionsManagerView.SelectedLanguage switch
-            {
-                SelectedLanguage.Jp => Resources.Load<LanguageHash>(LangAssetFolderPath + AssetNameJa),
-                SelectedLanguage.En => Resources.Load<LanguageHash>(LangAssetFolderPath + AssetNameEn),
-                _ => Lang
-            };
+            Lang = LoadLanguage((SelectedLanguage)FBXOptionsManagerView.SelectedLanguage);
         }
 
         public static void Localize()
         {
-            Lang = (SelectedLanguage)FBXOptionsManagerView.SelectedLanguage switch
-            {
-                SelectedLanguage.Jp => Resources.Load<LanguageHash>(LangAssetFolderPath + AssetNameJa),
-                SelectedLanguage.En => Resources.Load<LanguageHash>(LangAssetFolderPath + AssetNameEn),
-                _ => Lang
-            };
+            Lang = LoadLanguage((SelectedLanguage)FBXOptionsManagerView.SelectedLanguage);
         }
 
-        private enum SelectedLanguage
+        private static LanguageHash LoadLanguage(SelectedLanguage selectedLanguage)
         {
-            Jp,
-            En
+            return selectedLanguage switch
+            {
+                SelectedLanguage.Jp => Resources.Load<LanguageHash>(Path.Combine(LangAssetFolderPath, AssetNameJa)),
+                SelectedLanguage.En => Resources.Load<LanguageHash>(Path.Combine(LangAssetFolderPath, AssetNameEn)),
+                _ => Lang
+            };
         }
     }
 }
